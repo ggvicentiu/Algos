82ed4a7db1c12ee8b48450ed6fe6b334ae35e8e2
Georgian Grigore <georgiangrigore@Georgians-MacBook-Pro-2.local>
2020-11-16 21:22:30 -0500

    removed stop execution in main

--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -10,8 +10,6 @@ namespace Graphs
             DoTopologicalSort(14, new[] { new[] {0,2 }, new[] { 0, 3 }, new[] { 0, 6 }, new[] { 1,4 }, new[] { 2,6 },
             new[] {3,1 },new[] {3,4 },new[] {4,5 },new[] {4,8 },new[] {6,7 },new[] {6,11 },
             new[] {7,4},new[] {7, 12 },new[] {9,2 },new[] {9,10 },new[] {10,6 },new[] {11,12 }});
-
-            Console.ReadLine();
         }
 
         private static void DoTopologicalSort(int N, int[][] edges)
